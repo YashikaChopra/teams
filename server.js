33414b0f3b824d37b7d6ee233b72dc2af77fd8a6
yashika <yashikachopra09@gmail.com>
2021-06-22 19:59:18 +0530

    Mute, stop video feature

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 
 // rendering views, using ejs lib
 app.set('view engine', 'ejs')
 // setting up static folder -> all of js, css in *public* folder
 app.use(express.static('public'))
+
+app.use('/peerjs', peerServer)
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
